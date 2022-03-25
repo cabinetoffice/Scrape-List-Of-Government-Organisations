446abb1ada7008e29b63917bf58e5045b09ea2ed
Ashley Poole <ashley.poole@cabinetoffice.gov.uk>
2022-03-25 11:10:03 +0000

    Read html page, extract the organisations listed and clean them

--- run.R
+++ run.R
@@ -1,2 +1,18 @@
 ## This code scrapes all the organisations currently present on https://www.gov.uk/government/organisations and writes them to a dataset.
 
+library(magrittr)
+
+url <- "https://www.gov.uk/government/organisations"
+
+webpage <- rvest::read_html(url)
+
+raw_items <- 
+  webpage %>% 
+  rvest::html_elements(".organisations-list__item") %>% 
+  rvest::html_text()
+
+clean_items <- 
+  raw_items %>% 
+  stringr::str_trim() %>% 
+  stringr::str_squish() %>% 
+  stringr::str_replace(pattern = " Works with.*", replacement = "")
